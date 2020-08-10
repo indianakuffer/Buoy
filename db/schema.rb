f5c208e6faea2fb42ec2110b2e155b76c6cc78df
Indiana Kuffer <indianakuffer@gmail.com>
2020-08-10 11:12:33 -0400

    migrate likes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_150953) do
+ActiveRecord::Schema.define(version: 2020_08_10_151148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "thought_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["thought_id"], name: "index_likes_on_thought_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "profile_pics", force: :cascade do |t|
     t.string "image"
@@ -41,6 +50,8 @@ ActiveRecord::Schema.define(version: 2020_08_10_150953) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "likes", "thoughts"
+  add_foreign_key "likes", "users"
   add_foreign_key "profile_pics", "users"
   add_foreign_key "thoughts", "users"
 end
