a0b2e07ac6c838feadb7103b5a9b77c85288c835
Indiana Kuffer <indianakuffer@gmail.com>
2020-08-10 11:36:13 -0400

    migrate remove user_id from profile_pics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_152126) do
+ActiveRecord::Schema.define(version: 2020_08_10_153340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,8 @@ ActiveRecord::Schema.define(version: 2020_08_10_152126) do
 
   create_table "profile_pics", force: :cascade do |t|
     t.string "image"
-    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_profile_pics_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -63,6 +61,5 @@ ActiveRecord::Schema.define(version: 2020_08_10_152126) do
 
   add_foreign_key "likes", "thoughts"
   add_foreign_key "likes", "users"
-  add_foreign_key "profile_pics", "users"
   add_foreign_key "thoughts", "users"
 end
