07a0eb23e329d55ee661698b4139eebd4137dbe1
Indiana Kuffer <indianakuffer@gmail.com>
2020-08-10 21:50:42 -0400

    add auth verification on app load

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { verifyUser } from './services/auth';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+
+  useEffect(() => { handleVerify() }, [])
+
+  const handleVerify = async () => {
+    const userData = await verifyUser()
+    setCurrentUser(userData)
+  }
+
   return (
     <div>
 
