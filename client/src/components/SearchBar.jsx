78ca8e4dd4aa02d4011b0179f1cb0e4f6cf96c0b
Indiana Kuffer <indianakuffer@gmail.com>
2020-08-11 21:09:27 -0400

    add colors to search

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -5,24 +5,42 @@ const SearchBarContainer = styled.div`
   display: flex;
   background-color: white;
 `
+const Circle = styled.div`
+  background-color: #${props => props.color};
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+`
 
 export default function SearchBar(props) {
-  const [formData, setFormData] = useState({ search: '', })
+  const [formData, setFormData] = useState('')
+  const [colorList, setColorList] = useState(['e64c3c', 'f0c419', '086788', 'fbffe2', '2a9d8f'])
+  const [selectedColors, setSelectedColors] = useState([])
 
   const handleChange = (e) => {
-    setFormData({ search: e.target.value })
+    setFormData(e.target.value)
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      if (formData.search) {
-        props.filterThoughts([], formData.search.split(' '))
-      } else {
+      let formArray = formData.split(' ')
+      if (!formData) { formArray = [] }
+      if (!formData && selectedColors.length <= 0) {
         props.fetchThoughts()
+      } else {
+        props.filterThoughts(selectedColors, formArray)
       }
     } catch (error) {
       alert(error)
+    }
+  }
+
+  const toggleColor = (hex) => {
+    if (selectedColors.includes(hex)) {
+      setSelectedColors(selectedColors.filter(color => color !== hex))
+    } else {
+      setSelectedColors([...selectedColors, hex])
     }
   }
 
@@ -30,9 +48,12 @@ export default function SearchBar(props) {
     <SearchBarContainer>
       <form onSubmit={handleSubmit}>
         <label htmlFor='search'>
-          <input type='text' name='search' value={formData.search} onChange={handleChange} placeholder='search...'></input>
+          <input type='text' name='search' value={formData} onChange={handleChange} placeholder='search...'></input>
         </label>
       </form>
+      {colorList.map(color => (
+        <Circle color={color} onClick={() => toggleColor(color)}></Circle>
+      ))}
     </SearchBarContainer>
   )
 }
