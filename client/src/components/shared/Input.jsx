ab29ba4caec0fadbd6e443ee9f5a8870c54a393d
Indiana Kuffer <indianakuffer@gmail.com>
2020-08-12 14:55:47 -0400

    style login screen

--- client/src/components/shared/Input.jsx
+++ client/src/components/shared/Input.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export default function Input() {
+const StyledInput = styled.input`
+  border: none;
+  width: 100%;
+  border-radius: 10px;
+  font-size: 22px;
+  padding: 5px 20px;
+  outline: none;
+  color: #086788;
+  &::placeholder {
+    color: lightgrey;
+  }
+`
+
+export default function Input(props) {
   return (
-    <div>
-
-    </div>
+    <StyledInput
+      type={props.type}
+      name={props.name}
+      value={props.value}
+      onChange={props.onChange}
+      placeholder={props.placeholder}
+    />
   )
 }
